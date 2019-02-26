f38ef3afbd748fb42edbb0e9403981d042bbef35
Rachel Vancleave <sindiewen@gmail.com>
2019-02-26 14:50:34 -0800

    Added project and .gitignore

---
+++ AI Club Presentation/Assets/Scripts/MoveCube.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+
+    public float speedx;
+    public float speedz;
+
+    private Vector3 movement;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Use this for initialization
+    private void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.z = Input.GetAxis("Vertical");
+
+        transform.Translate(movement.x * speedx, 0, movement.z * speedz);
+        Debug.Log("Moving " + movement.x * speedx + " " + movement.z * speedz);
+	}
+}
